e25ccd4207df62caa1d16c5c10a61c0695e8cd95
Artur Gramacki <89815117+artur-gramacki@users.noreply.github.com>
2026-03-10 18:17:10 +0100

    file updated

--- R/package.R
+++ R/package.R
@@ -1,10 +1,16 @@
 #' Processing Time Series Data Using the Matching Pursuit Algorithm
 #'
-#' Dłuższy opis pakietu. TODO.
+#' This package provides tools for analysing and decomposing time series data using the \strong{Matching Pursuit (MP)} algorithm, a greedy signal decomposition technique that represents complex signals as a linear combination of simpler functions selected from an overcomplete dictionary.
+#'
+#' @details
+#' In addition to working with generic time-series data, the package also supports direct loading of data stored in EDF and EDF(+) files. These formats are widely used for storing physiological signals such as EEG, EMG, or ECG recordings. By enabling the import of EDF and EDF(+) files, the package facilitates the analysis of biomedical signals.
+#'
+#' In the package we use the implementation of the Matching Pursuit algorithm by Piotr Różański available at \url{https://github.com/develancer/empi}.
 #'
 #' @docType package
+#'
 #' @name MatchingPursuit
-#' @details todo
+#'
 #' @references
 #' Mallat, S. & Zhang, Z. (1993). \emph{Matching Pursuits with Time-Frequency Dictionaries}.
 #' IEEE Transactions on Signal Processing, vol. 41, no. 12, pp. 3397-3415, \url{https://doi.org/10.1109/78.258082}
